653c87dbf001671c74548d8de39d4817c3349c98
rjawaji <rjawaji@icloud.com>
2024-02-14 12:56:42 +1100

    week 1

---
+++ week1/src/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        /*
+         * Escape Sequences:
+         * \n - new line
+         * \t - indent line
+         * \\ - allows for using a backslash
+         * \" - allows for quotation in code
+         */
+        System.out.println("Hello world");
+        System.out.println("This is my first week of Java :)");
+        System.out.println("Look ath the next line \n here i am!");
+        System.out.println("Look over \t\t \"Here\"");
+        System.out.println("Look at this backslash \\ pretty cool right?");
+    }
+}
